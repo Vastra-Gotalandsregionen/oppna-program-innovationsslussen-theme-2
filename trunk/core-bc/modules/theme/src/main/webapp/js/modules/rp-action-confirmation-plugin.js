19f5c3d7050719ea3076962f58afb8ca5aeb6f8c
erik.andersson@monator.com <erik.andersson@monator.com@4d903503-ac1c-830f-496e-739a1acaa881>
2014-08-26 14:46:18 +0000

    Fixed rp-confirmation-plugin and other javascript. Only script that is not migrated is "rp-iframe-link-plugin" - but as far as I know this is not used for Innovationsslussen.

--- trunk/core-bc/modules/theme/src/main/webapp/js/modules/rp-action-confirmation-plugin.js
+++ trunk/core-bc/modules/theme/src/main/webapp/js/modules/rp-action-confirmation-plugin.js
@@ -1,124 +1,124 @@
 AUI.add('rp-action-confirmation-plugin', function(A) {
-	
+
 	var	Lang = A.Lang,
 		getClassName = A.ClassNameManager.getClassName,
 		NAME = 'rp-action-confirmation-plugin',
 		NS = 'rp-action-confirmation-plugin',
-		
+
 		DATA_CONFIRM_MSG = 'data-confirm-msg',
-		
+
 		TEXT_DEFAULT_MSG = '&Auml;r du s&auml;ker p&aring; att du vill g&ouml;ra detta?',
 		TEXT_DEFAULT_TITLE = '&Auml;r du s&auml;ker?',
-	
+
 		// Custom Attributes
 		WIDTH = 'width',
-	
+
 		// Property keys
 		HOST = 'host'
 	;
-	
+
 	var TPL_DIALOG_CONTENTS = '<div class="rp-action-confirmation-dialog-bd"></div>';
-	
+
 	var RpActionConfirmation = A.Component.create({
-		
+
 		ATTRS:
 			{
-	            width: {
-	            value: 600
-            }
-        },
+        width: {
+          value: 600
+        }
+      },
 
-        EXTENDS: A.Plugin.Base,
-        NAME: NAME,
-        NS: NS,
+      EXTENDS: A.Plugin.Base,
+      NAME: NAME,
+      NS: NS,
 
-        prototype: {
-        	dialog: null,
-        	msg: '',
-        	url: '',
+      prototype: {
+      	dialog: null,
+      	msg: '',
+      	url: '',
 
-			initializer: function() {
-				var instance = this;
-				
-				var host = instance.get(HOST);
-				
-				instance.url = host.getAttribute('href');
-				
-				if(host.hasAttribute(DATA_CONFIRM_MSG)) {
-					instance.msg = host.getAttribute(DATA_CONFIRM_MSG);
-				}
-				else {
-					instance.msg = TEXT_DEFAULT_MSG;
-				}
-				
-				//data-confirm-msg
+				initializer: function() {
+					var instance = this;
 
-				instance._initLinks();
-			},
+					var host = instance.get(HOST);
 
-			_initLinks: function() {
-				var instance = this;
-		
-				var host = instance.get(HOST);
-				
-				host.removeClass('requires-confirmation');
-				host.addClass('requires-confirmation-active');
-				
-				host.on('click', instance._onHostClick, instance);
-			},
-			
+					instance.url = host.getAttribute('href');
+
+					if(host.hasAttribute(DATA_CONFIRM_MSG)) {
+						instance.msg = host.getAttribute(DATA_CONFIRM_MSG);
+					}
+					else {
+						instance.msg = TEXT_DEFAULT_MSG;
+					}
+
+					//data-confirm-msg
+
+					instance._initLinks();
+				},
+
+				_initLinks: function() {
+					var instance = this;
+
+					var host = instance.get(HOST);
+
+					host.removeClass('requires-confirmation');
+					host.addClass('requires-confirmation-active');
+
+					host.on('click', instance._onHostClick, instance);
+				},
+
 			_onHostClick: function(e) {
 				var instance = this;
-				
+
 				e.halt();
-				
+
 				var width = 300;
 
 				var dialog = new A.Modal(
-                    {
-                        bodyContent: instance.msg,
-                        centered: true,
-                        headerContent: TEXT_DEFAULT_TITLE,
-                        modal: true,
-                        toolbars: {
-                            footer: [
-                                {
-                                    label: 'Ja',
-                                    on: {
-                                        click: function() {
-                                            instance._confirmAction();
-                                        }
-                                    }
-                                }, {
-                                    label: 'Avbryt',
-                                    on: {
-                                        click: function() {
-                                            instance._haltAction();
-                                        }
-                                    }
-                                }
-                            ]
-                        },
-                        width: width,
-                        zIndex: 1000
+          {
+            bodyContent: instance.msg,
+            centered: true,
+            headerContent: TEXT_DEFAULT_TITLE,
+            modal: true,
+            toolbars: {
+            	footer: [
+                {
+                  label: 'Ja',
+                  on: {
+                    click: function() {
+                    	instance._confirmAction();
                     }
-                );
+                  }
+                }, {
+                  label: 'Avbryt',
+                  on: {
+                  	click: function() {
+                    	instance._haltAction();
+                    }
+                  }
+                }
+              ]
+            },
+            width: width,
+            zIndex: 1000
+          }
+        );
 
-                dialog.render();
+        dialog.render();
 
 				instance.dialog = dialog;
 			},
-			
+
 			_confirmAction: function () {
 				var instance = this;
-				
+
 				// Should now continue with action
 				instance.dialog.hide();
 				if(instance.url != '') {
-					window.location.href = instance.url;	
+					window.location.href = instance.url;
 				}
 			},
-			
+
 			_haltAction: function () {
 				var instance = this;
 				instance.dialog.hide();
@@ -135,7 +135,7 @@ AUI.add('rp-action-confirmation-plugin', function(A) {
 	}, '1.0.1' ,{
 		requires:[
 			'aui-component',
-            'aui-modal',
+      'aui-modal',
 			'plugin'
 		]
 	}
