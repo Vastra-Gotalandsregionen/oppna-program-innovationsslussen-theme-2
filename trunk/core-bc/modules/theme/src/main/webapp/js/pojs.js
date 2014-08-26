c9b34799b02fe14b25b02fa161327b81bd7b5933
erik.andersson@monator.com <erik.andersson@monator.com@4d903503-ac1c-830f-496e-739a1acaa881>
2014-08-26 12:28:31 +0000

    Updated pojs.js

--- trunk/core-bc/modules/theme/src/main/webapp/js/pojs.js
+++ trunk/core-bc/modules/theme/src/main/webapp/js/pojs.js
@@ -1,8 +1,13 @@
 // Plain javascript that runs before AUI is ready (to prevent content flashing
 
-// Body
-addCssClass(document.body, 'js');
-bindNavigationTrigger(document.getElementById('navigationTrigger'));
+window.onload= onWindowLoad;
+
+function onWindowLoad() {
+	// Body
+	addCssClass(document.body, 'js');
+
+	//bindNavigationTrigger(document.getElementById('navigationTrigger'));
+}
 
 function hasCssClass(elem, className) {
 	if(elem) {
@@ -19,7 +24,7 @@ function addCssClass(elem, className) {
 }
 
 function removeCssClass(elem, className) {
-	
+
 	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
 	if (hasCssClass(elem, className)) {
 		while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
